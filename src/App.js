00cd8f9af684cdc3b2ea434c14d956cde9bae14c
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-09 19:36:32 -0400

    Create a stateless functional component and a React component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
-function App() {  
-  return (
-    // 2). Create a complex JSX element
-    <div>
-      {/* 1). Create a simple JSX element */}
-      <h1>Food</h1>
-      <p>We eat three meals a day</p>
-      {/* 3). Add comments in JSX with {} */}
-      {/* 5). Add a class with className */}
-      {/* 6). Self-closing JSX Tags */}
-      <br />
-      <ul className="myList">
-        <li>Breakfast</li>
-        <li>Lunch</li>
-        <li>Dinner</li>
-      </ul>
-      <hr />
-    </div>
-  );
-}
-    
+import React from 'react';
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
+// 7).Stateless functional component
+// function Apple() {  
+//   return (
+//     2). Create a complex JSX element
+//    <div>
+// {/* 1). Create a simple JSX element */}
+// <h1>Food</h1>
+//      <p>We eat three meals a day</p>
+//      {/* 3). Add comments in JSX with {} */}
+//      {/* 5). Add a class with className */}
+//      {/* 6). Self-closing JSX Tags */}
+//      <br />
+//      <ul className="myList">
+//        <li>Breakfast</li>
+//        <li>Lunch</li>
+//        <li>Dinner</li>
+//      </ul>
+//      <hr />
+//    </div>
+//  );
+//}
 
-export default App;
+// 8). Create a React component
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+      </div>
+    )
+  }
+};
+
+
+export default MyComponent;
