1b1a8351c51d1d65722fbe6735f37b9fc625aaca
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-11 11:11:04 -0400

    Render state in the UI another way

--- src/App.js
+++ src/App.js
@@ -320,5 +320,24 @@ class MyComponent extends React.Component {
 };
 
 
+// 22). Render state in the UI another way
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    const name = this.state.name;
+    return (
+      <div>
+        <h1>{name}</h1>
+      </div>
+    );
+  }
+};
+
+
 
 export default CampSite;
