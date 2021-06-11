5e39f6669cfd912bec520e3abdd9318af46a8092
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-11 13:11:12 -0400

    Create a controlled input

--- src/App.js
+++ src/App.js
@@ -444,7 +444,33 @@ class Counter extends React.Component {
 };
 
 
+// 26). Create a controlled input
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event){
+    this.setState({
+      input: event.target.value
+    }) 
+  }
+
+  render() {
+    return (
+      <div>
+        <input value={this.state.input} onChange={this.handleChange} />
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
 
 
 
-export default Counter;
+export default ControlledInput;
