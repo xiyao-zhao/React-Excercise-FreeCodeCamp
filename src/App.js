b3fdcbbd1556f2a8a58b8b3cb0ea11b9f3c6f931
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-11 13:09:16 -0400

    Write a simple counter

--- src/App.js
+++ src/App.js
@@ -368,7 +368,7 @@ class Component extends React.Component {
 
 
 // 24). Use state to toggle an element
-class MyComponent extends React.Component {
+/*class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -398,9 +398,53 @@ class MyComponent extends React.Component {
     }
   }
 }
+*/
+
+
+// 25). Write a simple counter
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  increment(){
+    this.setState(state => ({
+      count: state.count + 1
+    }))
+  };
+
+  decrement(){
+    this.setState(state => ({
+      count: state.count - 1
+    }))
+  };
+
+  reset() {
+    this.setState({
+      count: 0
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button className='inc' onClick={this.increment}>Increment!</button>
+        <button className='dec' onClick={this.decrement}>Decrement!</button>
+        <button className='reset' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+};
 
 
 
 
 
-export default Component;
+export default Counter;
