35b671f027a8e7ca24050f3f2a4f5b5b0b59a081
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-11 18:31:10 -0400

    Use the lifecycle method componentDidMount

--- src/App.js
+++ src/App.js
@@ -597,5 +597,30 @@ class RenderInput extends React.Component {
 };
 
 
+// 30). Use the lifecycle method componentDidMount
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Active Users: {this.state.activeUsers}</h1>
+      </div>
+    );
+  }
+}
+
+
 
 export default MyApp;
