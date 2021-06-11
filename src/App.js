0f8dca2a12eca436ddaf852fa74c3625cc2b94b8
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-11 15:44:41 -0400

    Create a controlled form

--- src/App.js
+++ src/App.js
@@ -472,5 +472,44 @@ class ControlledInput extends React.Component {
 };
 
 
+// 27). Create a controlled form
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-export default ControlledInput;
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      submit: this.state.input
+    })
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.input} onChange={this.handleChange}/>
+          <button type='submit'>Submit!</button>
+        </form>
+        <h1>{this.state.submit}</h1>
+      </div>
+    );
+  }
+}
+
+
+
+export default MyForm;
