9bfe5bdf041bb4d9d8ff1c13a84d44d2e4ae50b6
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-10 20:13:54 -0400

    Use PropTypes to define props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 // 7).Stateless functional component
-// function Apple() {  
-//   return (
+ function Apple() {  
+   return (
 //     2). Create a complex JSX element
-//    <div>
-// {/* 1). Create a simple JSX element */}
-// <h1>Food</h1>
-//      <p>We eat three meals a day</p>
-//      {/* 3). Add comments in JSX with {} */}
-//      {/* 5). Add a class with className */}
-//      {/* 6). Self-closing JSX Tags */}
-//      <br />
-//      <ul className="myList">
-//        <li>Breakfast</li>
-//        <li>Lunch</li>
-//        <li>Dinner</li>
-//      </ul>
-//      <hr />
-//    </div>
-//  );
-//}
+    <div>
+ {/* 1). Create a simple JSX element */}
+ <h1>Food</h1>
+      <p>We eat three meals a day</p>
+      {/* 3). Add comments in JSX with {} */}
+      {/* 5). Add a class with className */}
+      {/* 6). Self-closing JSX Tags */}
+      <br />
+      <ul className="myList">
+        <li>Breakfast</li>
+        <li>Lunch</li>
+        <li>Dinner</li>
+      </ul>
+      <hr />
+    </div>
+  );
+}
 
 
 // 8). Create a React component
-/*class MyComponent extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -48,7 +48,7 @@ const MyChildComponent = () => {
     </div>
   );
 };
-*/
+
 
 
 // 10). Use React to render nested components
@@ -223,6 +223,10 @@ const Items = (props) => {
     </div>
   )
 };
+
+// 17). Use PropTypes to Define the Props
+Items.propTypes = { quantity: PropTypes.number.isRequired }
+
 Items.defaultProps = { Quantity: 0 };
 
 // 16). Override default props
