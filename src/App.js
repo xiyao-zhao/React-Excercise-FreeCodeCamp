eb11fea045e292850b4828eef42b83dd7dc8945b
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-12 14:40:59 -0400

    Give sibling elements a unique key attribute

--- src/App.js
+++ src/App.js
@@ -1044,5 +1044,28 @@ class MyToDoList extends React.Component {
 }
 
 
+// 42). Give sibling elements a unique key attribute
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map(a => <li key={a}>{a}</li>); 
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
+
+
 
 export default MyToDoList;
