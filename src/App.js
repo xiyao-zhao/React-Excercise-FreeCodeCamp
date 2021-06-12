1980975c94c185b0af1932a81f81c67eeafff2e2
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-12 14:20:55 -0400

    Render conditionally from props

--- src/App.js
+++ src/App.js
@@ -732,7 +732,7 @@ class Colorful extends React.Component {
 
 
 // 35). Use advanced Javascript in React render method
-const inputStyle = {
+/*const inputStyle = {
   width: 235,
   margin: 5
 };
@@ -801,6 +801,7 @@ class MagicEightBall extends React.Component {
     );
   }
 }
+*/
 
 
 // 36). Render with an if-else condition
@@ -918,5 +919,45 @@ class CheckUserAge extends React.Component {
 }
 
 
+// 39). Render conditionally from props
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <h1>
+      {this.props.fiftyFifty === true ? "You Win!" : "You Lose!"}
+      </h1>
+    )
+  }
+}
+
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+        counter: this.state.counter + 1
+    });
+  }
+  render() {
+    const expression = Math.random() >= .5 ? true : false; 
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        <Results fiftyFifty={expression}/>
+        <p>{'Turn: ' + this.state.counter}</p>
+      </div>
+    );
+  }
+}
+
+
   
-export default CheckUserAge;
+export default GameOfChance;
